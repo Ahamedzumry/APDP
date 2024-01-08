12bf5d54c7d0bb438308a5ef997edb8d3b51cf08
lapto <1024534@bcas.ac>
2024-01-08 13:22:49 +0530

    add

--- OOP.py
+++ OOP.py
@@ -1,24 +1,12 @@
-class User:
-    def _init_(self, username, password):
-        self.username = username
-        self.password = password
-
-def authenticate(username, password, users):
-    for user in users:
-        if user.username == username and user.password == password:
-            return True
-    return False
-
-# Example usage:
-user1 = User("john_doe", "password123")
-user2 = User("alice_smith", "securepass")
-
-user_list = [user1, user2]
-
-input_username = input("Enter your username: ")
-input_password = input("Enter your password: ")
-
-if authenticate(input_username, input_password, user_list):
-    print("Authentication successful!")
-else:
-    print("Authentication failed.")
+class Employee:
+    def _init_(self, name, salary):
+        self.name = name
+        self.salary = salary
+    def show(self):
+        print("name is ", self.name,"and salary is ",self.__salary)
+# outside class
+E = Employee("Abdullah",95000)
+E.PrintName()
+print(E.name)
+print(E.printName())
+print(E.__salary)
